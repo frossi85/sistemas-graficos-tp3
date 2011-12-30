597aeaf64e62cfefe99276cee5710344f6b355d8
gustavo.g.negri@gmail.com <gustavo.g.negri@gmail.com@475089f5-3604-efdd-6849-81d60c1289cb>
2011-12-30 22:55:07 +0000

    Se completaron clases del dominio

--- trunk/src/objetosEscena/Empaquetador.java
+++ trunk/src/objetosEscena/Empaquetador.java
@@ -4,6 +4,17 @@ import utilidades.Dibujable;
 
 public class Empaquetador implements Dibujable {
 
+	int cantidadBotellasRecibidas;
+	
+	public void recibirBotella(Botella botella){
+		this.cantidadBotellasRecibidas++;
+		
+		if(this.cantidadBotellasRecibidas == 4){
+			//genero caja armada de botellas y se la paso a la rampa
+			this.cantidadBotellasRecibidas = 0;
+		}
+	}
+	
 	@Override
 	public void dibujar() {
 		// TODO Auto-generated method stub
--- trunk/src/objetosEscena/Rellenador.java
+++ trunk/src/objetosEscena/Rellenador.java
@@ -4,6 +4,9 @@ import utilidades.Dibujable;
 
 public class Rellenador implements Dibujable {
 
+	
+	public void liberarLiquido(){}
+	
 	@Override
 	public void dibujar() {
 		// TODO Auto-generated method stub
