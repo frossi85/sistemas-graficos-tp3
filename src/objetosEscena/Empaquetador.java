12cf9fb484d30c7515812132b10c449eb4a876b4
gustavo.g.negri <gustavo.g.negri@gmail.com>
2012-01-02 18:00:19 +0000

    Cuando recibe cuatro botellas notifica a observadores que su estado cambio.

--- src/objetosEscena/Empaquetador.java
+++ src/objetosEscena/Empaquetador.java
@@ -8,18 +8,27 @@ import utilidades.LineaProduccion;
 public class Empaquetador extends Observable implements Dibujable {
 
 	int cantidadBotellasRecibidas;
+	public static int CAPACIDAD_BOTELLAS = 4;
 	
 	public Empaquetador(LineaProduccion linea,Rampa rampa){
+		this.cantidadBotellasRecibidas = 0;
 		addObserver(linea);
 		addObserver(rampa);
 	}
 	public void recibirBotella(Botella botella){
 		this.cantidadBotellasRecibidas++;
-		
+		setChanged();
+        notifyObservers();
+        clearChanged();
 		if(this.cantidadBotellasRecibidas == 4){
 			//genero caja armada de botellas y se la paso a la rampa
 			this.cantidadBotellasRecibidas = 0;
+			
 		}
+	}
+	
+	public int getCantidadBotellasRecibidas(){
+		return this.cantidadBotellasRecibidas;
 	}
 	
 	@Override
