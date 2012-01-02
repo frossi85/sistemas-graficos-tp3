feab79f7a350b912aa010f45e1cd675041e811de
gustavo.g.negri <gustavo.g.negri@gmail.com>
2012-01-02 18:01:37 +0000

    Ahora tiene una posicion. Esto sirve para comparar con la posicion de la botella y saber si la botella llego a la posicion de Etiquetado

--- src/objetosEscena/Etiquetador.java
+++ src/objetosEscena/Etiquetador.java
@@ -4,11 +4,13 @@ import java.util.Observable;
 
 import utilidades.Dibujable;
 import utilidades.LineaProduccion;
+import utilidades.Vertice;
 
 public class Etiquetador extends Observable implements Dibujable {
 	
 	private float tiempoEtiquetado;
 	private boolean terminoEtiquetado;
+	private Vertice posicion;
 	
 	public Etiquetador(LineaProduccion linea, float tiempoEtiquetado){
 		this.tiempoEtiquetado = tiempoEtiquetado;
