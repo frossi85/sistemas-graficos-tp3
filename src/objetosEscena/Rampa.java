bae7d8da4ae6aac5f804a13549f87a87694eafaa
gustavo.g.negri <gustavo.g.negri@gmail.com>
2012-01-02 03:30:44 +0000

    Agregado patron observador

--- src/objetosEscena/Rampa.java
+++ src/objetosEscena/Rampa.java
@@ -1,18 +1,31 @@
 package objetosEscena;
 
 import utilidades.Dibujable;
+import utilidades.LineaProduccion;
 
-public class Rampa implements Dibujable{
+import java.util.Observable;
+import java.util.Observer;
+
+public class Rampa extends Observable implements Dibujable,Observer{
 	
 	
 	public void setPendiente(float angulo){}
 	public void setLargo(float largo){}
 	public void setAncho(float ancho){}
 	
+	public Rampa(LineaProduccion linea){
+		addObserver(linea);
+	}
+	
 	public void recibirPackBotellas(){}
 
 	@Override
 	public void dibujar() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void update(Observable o, Object arg) {
 		// TODO Auto-generated method stub
 		
 	}
