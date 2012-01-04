2e3b9e4314cf968039fa6fa35883d90c4bcfca4e
gustavo.g.negri@gmail.com <gustavo.g.negri@gmail.com@475089f5-3604-efdd-6849-81d60c1289cb>
2012-01-04 18:40:59 +0000

    Agregado del creador, variables estaticas para setear valores de la produccion, seteo de timer e inicio del metodo Producir (quedo en TODO)

--- trunk/src/utilidades/LineaProduccion.java
+++ trunk/src/utilidades/LineaProduccion.java
@@ -18,6 +18,35 @@ public class LineaProduccion implements Observer{
 	private Rellenador rellenador;
 	private Empaquetador empaquetador;
 	private Rampa rampa;
+	private float timer = 0.0f;
+	private static float AVANCE_TIEMPO = 0.1f;
+	private static float VELOCIDAD_CINTA = 5f;
+	private static int CAPACIDAD_CINTA = 10;
+	private static float TIEMPO_ETIQUETADO = 3f;
+	private static float AVANCE_BOTELLAS = VELOCIDAD_CINTA*AVANCE_TIEMPO;  //distancia = tiempo*velocidad
+	
+	
+	public LineaProduccion(){
+		this.cinta = new CintaTransportadora(CAPACIDAD_CINTA, this, VELOCIDAD_CINTA, AVANCE_BOTELLAS);
+		this.rampa = new Rampa(this);
+		this.empaquetador = new Empaquetador(this,rampa);
+		this.etiquetador = new Etiquetador(this, TIEMPO_ETIQUETADO);
+		this.expededoraBotellas = new Dispenser(this);
+		this.rellenador = new Rellenador(this);
+	}
+	
+	public void avanzarTiempo(){
+		this.timer += AVANCE_TIEMPO;
+		
+	}
+	
+	public void Producir(){  // TODO este metodo mueve toda la produccion
+		if(!this.cinta.estaLlenaDeBotellas()){
+			this.cinta.recibirBotella(this.expededoraBotellas.entregarBotella());
+			this.cinta.avanzarCinta();
+			
+		}
+	}
 	@Override
 	public void update(Observable o, Object arg) {
 		// TODO Auto-generated method stub
