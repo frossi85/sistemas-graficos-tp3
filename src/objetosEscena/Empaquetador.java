2bb49241566c4eb156e2596abf8c729b011b49a3
gustavo.g.negri <gustavo.g.negri@gmail.com>
2012-01-06 20:03:50 +0000

    dibujar() imprime texto

--- src/objetosEscena/Empaquetador.java
+++ src/objetosEscena/Empaquetador.java
@@ -33,7 +33,7 @@ public class Empaquetador extends Observable implements Dibujable {
 	
 	@Override
 	public void dibujar() {
-		// TODO Auto-generated method stub
+		System.out.println("Se dibujo empaquetador");
 
 	}
 
