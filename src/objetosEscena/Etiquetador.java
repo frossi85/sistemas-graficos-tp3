4cab29289934d2a830514b543f5195c3f6061a43
gustavo.g.negri <gustavo.g.negri@gmail.com>
2012-01-06 19:57:27 +0000

    ahora es animable y tiene setPosicion()

--- src/objetosEscena/Etiquetador.java
+++ src/objetosEscena/Etiquetador.java
@@ -2,11 +2,12 @@ package objetosEscena;
 
 import java.util.Observable;
 
+import utilidades.Animable;
 import utilidades.Dibujable;
 import utilidades.LineaProduccion;
 import utilidades.Vertice;
 
-public class Etiquetador extends Observable implements Dibujable {
+public class Etiquetador extends Observable implements Dibujable,Animable {
 	
 	private float tiempoEtiquetado;
 	private boolean terminoEtiquetado;
@@ -21,9 +22,23 @@ public class Etiquetador extends Observable implements Dibujable {
 	
 	//TODO: No se si es necesario
 	public boolean terminoDeEtiquetar(){return terminoEtiquetado;}
-
+	
+	public Vertice getPosicion(){
+		return this.posicion;
+	}
+	
+	public void setPosicion(Vertice vert){
+		this.posicion = vert;
+	}
+	
 	@Override
 	public void dibujar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void animar() {
 		// TODO Auto-generated method stub
 		
 	}
