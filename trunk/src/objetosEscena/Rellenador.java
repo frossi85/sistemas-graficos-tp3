fea34ed81cbde0e89d59c554d7c9e06454969a6e
gustavo.g.negri@gmail.com <gustavo.g.negri@gmail.com@475089f5-3604-efdd-6849-81d60c1289cb>
2012-01-06 19:56:05 +0000

    ahora es animable y tiene metodo setPosicion()

--- trunk/src/objetosEscena/Rellenador.java
+++ trunk/src/objetosEscena/Rellenador.java
@@ -2,11 +2,12 @@ package objetosEscena;
 
 import java.util.Observable;
 
+import utilidades.Animable;
 import utilidades.Dibujable;
 import utilidades.LineaProduccion;
 import utilidades.Vertice;
 
-public class Rellenador extends Observable implements Dibujable {
+public class Rellenador extends Observable implements Dibujable,Animable {
 
 	private Vertice posicion;
 	public void liberarLiquido(){}
@@ -15,10 +16,24 @@ public class Rellenador extends Observable implements Dibujable {
 		addObserver(linea);
 	}
 	
+	public Vertice getPosicion(){
+		return this.posicion;
+	}
+	
+	public void setPosicion(Vertice vert){
+		this.posicion = vert;
+	}
+	
 	@Override
 	public void dibujar() {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void animar() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
