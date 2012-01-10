d4b2b0de024cbab386699bbb7fa1474e3edde608
gustavo.g.negri <gustavo.g.negri@gmail.com>
2012-01-10 23:16:00 +0000

    Se inicializo la posicion

--- src/objetosEscena/Dispenser.java
+++ src/objetosEscena/Dispenser.java
@@ -4,15 +4,23 @@ import java.util.Observable;
 
 import utilidades.Dibujable;
 import utilidades.LineaProduccion;
+import utilidades.Vertice;
 
 public class Dispenser extends Observable implements Dibujable {
-
+	
+	private Vertice posicion;
+	
 	public Dispenser(LineaProduccion linea){
 	addObserver(linea);
+	this.posicion = new Vertice(0f,0f,0f);
 	}
 	
 	public Botella entregarBotella(){
-		return new Botella(null);
+		return new Botella();
+	}
+	
+	public Vertice getPosicon(){
+		return this.posicion;
 	}
 	
 	@Override
