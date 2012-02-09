3e5898e7b480fc422aadd30104436a2e56b9acb1
gustavo.g.negri <gustavo.g.negri@gmail.com>
2012-02-09 23:54:34 +0000

    dibujar() recibe parametro

--- src/objetosEscena/Techo.java
+++ src/objetosEscena/Techo.java
@@ -1,11 +1,13 @@
 package objetosEscena;
 
+import javax.media.opengl.GLAutoDrawable;
+
 import utilidades.Dibujable;
 
 public class Techo implements Dibujable {
 
 	@Override
-	public void dibujar() {
+	public void dibujar(GLAutoDrawable gLDrawable) {
 		// TODO Auto-generated method stub
 
 	}
