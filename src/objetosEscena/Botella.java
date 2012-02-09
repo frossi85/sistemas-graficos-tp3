5239d5cc8fe7ec427c0b6e47190a04890a28f394
gustavo.g.negri <gustavo.g.negri@gmail.com>
2012-02-09 23:36:23 +0000

    Completando el metodo dibujar()

--- src/objetosEscena/Botella.java
+++ src/objetosEscena/Botella.java
@@ -1,6 +1,14 @@
 package objetosEscena;
 import java.util.Observable;
 
+import javax.media.opengl.GL;
+import javax.media.opengl.GL2;
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.glu.GLU;
+import javax.media.opengl.glu.GLUquadric;
+
+import com.jogamp.opengl.util.gl2.GLUT;
+
 import utilidades.Animable;
 import utilidades.Dibujable;
 import utilidades.LineaProduccion;
@@ -11,7 +19,7 @@ public class Botella  implements Dibujable,Animable {
 	private boolean lleno;
 	private boolean etiquetado;
 	private float porcentajeLlenado;
-	private static float altura = 3.0f;
+	private static float altura = 1.0f;
 	private Vertice posicion;
 	
 	
@@ -64,9 +72,17 @@ public class Botella  implements Dibujable,Animable {
 	}
 
 	@Override
-	public void dibujar() {
+	public void dibujar(GLAutoDrawable gLDrawable) {
 		System.out.println("Se dibujo botella");
+		//GL2 gl = gLDrawable.getGL().getGL2();
+		GLU glu = new GLU(); 
+		GLUquadric qobj0 = glu.gluNewQuadric();
 		
+		glu.gluQuadricDrawStyle(qobj0, GLU.GLU_FILL);
+		glu.gluQuadricNormals(qobj0, GLU.GLU_SMOOTH);
+		glu.gluCylinder(qobj0, 0.5f, 0.5f, this.altura, 15, 5);
+		//glu.gluDeleteQuadric(qobj0);
+			
 	}
 
 	@Override
