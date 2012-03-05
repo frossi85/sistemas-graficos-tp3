35f8846dfc40e9ed592cd1518239553300584278
gustavo.g.negri@gmail.com <gustavo.g.negri@gmail.com@475089f5-3604-efdd-6849-81d60c1289cb>
2012-03-05 07:56:32 +0000

    Se dibuja en pantalla

--- trunk/src/objetosEscena/Dispenser.java
+++ trunk/src/objetosEscena/Dispenser.java
@@ -2,7 +2,10 @@ package objetosEscena;
 
 import java.util.Observable;
 
+import javax.media.opengl.GL2;
 import javax.media.opengl.GLAutoDrawable;
+
+import com.jogamp.opengl.util.gl2.GLUT;
 
 import utilidades.Dibujable;
 import utilidades.LineaProduccion;
@@ -11,10 +14,11 @@ import utilidades.Vertice;
 public class Dispenser extends Observable implements Dibujable {
 	
 	private Vertice posicion;
+	private GLUT glut = new GLUT();
 	
 	public Dispenser(LineaProduccion linea){
 	addObserver(linea);
-	this.posicion = new Vertice(0f,0f,0f);
+	this.posicion = new Vertice(-0.25f,0f,0f);
 	}
 	
 	public Botella entregarBotella(){
@@ -28,6 +32,11 @@ public class Dispenser extends Observable implements Dibujable {
 	@Override
 	public void dibujar(GLAutoDrawable gLDrawable) {
 		System.out.println("Se dibujo dispenser");
+		final GL2 gl = gLDrawable.getGL().getGL2();
+		gl.glPushMatrix();
+			gl.glTranslatef(this.posicion.getX(), 0.0f, 0.0f);
+			glut.glutSolidCube(0.5f);
+		gl.glPopMatrix();	
 
 	}
 
