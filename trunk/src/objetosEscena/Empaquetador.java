3f2023a4e9d0d579f3e7bd5b530cfeeed8f54afd
gustavo.g.negri@gmail.com <gustavo.g.negri@gmail.com@475089f5-3604-efdd-6849-81d60c1289cb>
2012-03-05 22:50:00 +0000

    Se comento el mensaje al dibujarse, se le agrego una posicion y metodos seters y geters de la misma

--- trunk/src/objetosEscena/Empaquetador.java
+++ trunk/src/objetosEscena/Empaquetador.java
@@ -6,17 +6,29 @@ import javax.media.opengl.GLAutoDrawable;
 
 import utilidades.Dibujable;
 import utilidades.LineaProduccion;
+import utilidades.Vertice;
 
 public class Empaquetador extends Observable implements Dibujable {
 
 	int cantidadBotellasRecibidas;
+	private Vertice posicion;
 	public static int CAPACIDAD_BOTELLAS = 4;
 	
 	public Empaquetador(LineaProduccion linea,Rampa rampa){
 		this.cantidadBotellasRecibidas = 0;
+		this.posicion = new Vertice(9f,0f,0f);
 		addObserver(linea);
 		addObserver(rampa);
 	}
+	
+	public Vertice getPosicion(){
+		return this.posicion;
+	}
+	
+	public void setPosicion(Vertice vert){
+		this.posicion = vert;
+	}
+	
 	public void recibirBotella(Botella botella){	// cuando recibo botellas notifico de cambio a observadores
 		this.cantidadBotellasRecibidas++;
 		if(this.cantidadBotellasRecibidas == 4){
@@ -34,7 +46,7 @@ public class Empaquetador extends Observable implements Dibujable {
 	
 	@Override
 	public void dibujar(GLAutoDrawable gLDrawable) {
-		System.out.println("Se dibujo empaquetador");
+		//System.out.println("Se dibujo empaquetador");
 
 	}
 
