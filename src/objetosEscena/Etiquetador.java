18f39f5c39a79c2cc9c9fe800bc2cf45a07c7026
gustavo.g.negri <gustavo.g.negri@gmail.com>
2012-03-07 00:25:03 +0000

    Se cambio su posicion y se dibuja en pantalla

--- src/objetosEscena/Etiquetador.java
+++ src/objetosEscena/Etiquetador.java
@@ -2,7 +2,10 @@ package objetosEscena;
 
 import java.util.Observable;
 
+import javax.media.opengl.GL2;
 import javax.media.opengl.GLAutoDrawable;
+
+import com.jogamp.opengl.util.gl2.GLUT;
 
 import utilidades.Animable;
 import utilidades.Dibujable;
@@ -14,11 +17,12 @@ public class Etiquetador extends Observable implements Dibujable,Animable {
 	private float tiempoEtiquetado;
 	private boolean terminoEtiquetado;
 	private Vertice posicion;
+	private GLUT glut = new GLUT();
 	
 	public Etiquetador(LineaProduccion linea, float tiempoEtiquetado){
 		this.tiempoEtiquetado = tiempoEtiquetado;
 		addObserver(linea);
-		this.posicion = new Vertice(3f,0f,0f);
+		this.posicion = new Vertice(6f,0f,0f);
 	}
 	
 	public void etiquetar(){}
@@ -37,7 +41,11 @@ public class Etiquetador extends Observable implements Dibujable,Animable {
 	@Override
 	public void dibujar(GLAutoDrawable gLDrawable) {
 		//System.out.println("Se dibujo etiquetador");
-		
+		final GL2 gl = gLDrawable.getGL().getGL2();
+		gl.glPushMatrix();
+			gl.glTranslatef(this.posicion.getX(), 1f, 0.0f);
+			glut.glutSolidCube(0.9f);
+		gl.glPopMatrix();
 	}
 
 	@Override
