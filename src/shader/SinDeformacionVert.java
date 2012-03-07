b2876ec44e7a8b19f4d1bb79d460a33d27601bc0
frossi85 <frossi85@gmail.com>
2012-03-07 05:09:23 +0000

    -Faltaban commitear clases para la prueba de la luz y el uso de shaders

---
+++ src/shader/SinDeformacionVert.java
@@ -0,0 +1,42 @@
+package shader;
+
+import javax.media.opengl.GL2;
+import javax.media.opengl.GLAutoDrawable;
+
+public class SinDeformacionVert extends VertexShader{
+	private String filename = "SinDeformacion.vert";
+	private boolean parado = false;
+	
+	public void bind(GLAutoDrawable gLDrawable,int shaderprogram){
+		
+		GL2 gl_shader = gLDrawable.getGL().getGL2();
+	}
+
+	@Override
+	public void displayVertexAttrib() {
+	}
+
+	@Override
+	public void displayUniform() {
+
+	}
+
+	@Override
+	public String getFileName() {
+		return this.filename;
+	}
+	
+	public void pararanimacion() {
+		parado = true;		
+	}
+	
+	public void reiniciaranimacion(){
+		parado = false;
+	}
+	
+	
+	
+	
+	
+
+}
