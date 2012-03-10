5f61dcd1b6a74ee3560ceb18b0b6ed00fb07c778
gustavo.g.negri@gmail.com <gustavo.g.negri@gmail.com@475089f5-3604-efdd-6849-81d60c1289cb>
2012-03-10 05:13:21 +0000

    modifico getX y getY ademas de cambiar las bases.

--- trunk/src/utilidades/BezierCubica.java
+++ trunk/src/utilidades/BezierCubica.java
@@ -11,19 +11,19 @@ public class BezierCubica extends Curva {
 	}
 	
 	private float getB0(float u){
-		return (float) (Math.pow(u, 0f)*Math.pow(1f-u,3f));
+		return (float) (Math.pow(1f-u,3f));
 	}
 	
 	private float getB1(float u){
-		return (float) (6f/(2f)*Math.pow(u, 1f)*Math.pow(1f-u,2f));
+		return (float) (3f*u*Math.pow(1f-u,2f));
 	}
 	
 	private float getB2(float u){
-		return (float) (6f/(2f)*Math.pow(u, 2f)*Math.pow(1f-u,1f));
+		return (float) (3f*Math.pow(u, 2f)*(1f-u));
 	}
 	
 	private float getB3(float u){
-		return (float) (Math.pow(u, 3f)*Math.pow(1f-u,0f));
+		return (float) (Math.pow(u, 3f));
 	}
 	
 	
@@ -32,10 +32,7 @@ public class BezierCubica extends Curva {
 			return this.codError;
 		}
 		else{
-			return (float) (this.puntosDeControl.get(0).getX()*Math.pow(1f-u,3f)/Math.pow(this.getB0(u),3f) +
-		this.puntosDeControl.get(1).getX()*3f*u*Math.pow(1-u,2f)/Math.pow(this.getB1(u),3f)+
-		this.puntosDeControl.get(2).getX()*3f*Math.pow(u, 2)*(1f-u)/Math.pow(this.getB2(u), 3)+
-		this.puntosDeControl.get(3).getX()*Math.pow(u, 3f)/Math.pow(this.getB3(u), 3f));
+			return (float) (this.puntosDeControl.get(0).getX()*getB0(u)+puntosDeControl.get(1).getX()*getB1(u)+puntosDeControl.get(2).getX()*getB2(u)+puntosDeControl.get(3).getX()*getB3(u));
 		}
 	}
 	
@@ -44,10 +41,7 @@ public class BezierCubica extends Curva {
 			return this.codError;
 		}
 		else{
-			return (float) (this.puntosDeControl.get(0).getY()*Math.pow(1f-u,3f)/Math.pow(this.getB0(u),3f) +
-		this.puntosDeControl.get(1).getY()*3f*u*Math.pow(1-u,2f)/Math.pow(this.getB1(u),3f)+
-		this.puntosDeControl.get(2).getY()*3f*Math.pow(u, 2)*(1f-u)/Math.pow(this.getB2(u), 3)+
-		this.puntosDeControl.get(3).getY()*Math.pow(u, 3f)/Math.pow(this.getB3(u), 3f));
+			return (float) (this.puntosDeControl.get(0).getY()*getB0(u)+puntosDeControl.get(1).getY()*getB1(u)+puntosDeControl.get(2).getY()*getB2(u)+puntosDeControl.get(3).getY()*getB3(u));
 		}
 	}
 }
