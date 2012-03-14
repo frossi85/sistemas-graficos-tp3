53aa3be164b4d6e6f03db19e97105ec6b45022c4
gustavo.g.negri <gustavo.g.negri@gmail.com>
2012-03-14 18:44:15 +0000

    Se agregaron los puntos de control que formar la curva de Bezier de la botella. Botella ahora tiene una curva de Bezier.

--- src/objetosEscena/Botella.java
+++ src/objetosEscena/Botella.java
@@ -1,4 +1,5 @@
 package objetosEscena;
+import java.util.ArrayList;
 import java.util.Observable;
 
 import javax.media.opengl.GL;
@@ -12,7 +13,10 @@ import com.jogamp.opengl.util.gl2.GLUT;
 import utilidades.Animable;
 import utilidades.Dibujable;
 import utilidades.LineaProduccion;
+import utilidades.PuntoDeControl;
+import utilidades.SuperficieDeRevolucion;
 import utilidades.Vertice;
+
 
 
 
@@ -21,6 +25,7 @@ public class Botella  implements Dibujable,Animable {
 	private boolean etiquetado;
 	private float porcentajeLlenado;
 	private static float altura = 1.0f;
+	private SuperficieDeRevolucion sup;
 	private Vertice posicion;	// posicion relativa a la cinta transportadora
 	private GLUT glut = new GLUT();
 	
@@ -29,6 +34,30 @@ public class Botella  implements Dibujable,Animable {
 		this.lleno = false;
 		this.etiquetado = false;
 		this.posicion = new Vertice(0f,0f,0f);
+		ArrayList<PuntoDeControl>list = new ArrayList<PuntoDeControl>();
+				
+		//ult1
+		PuntoDeControl punto1 = new PuntoDeControl(1.7f,3f);
+		PuntoDeControl punto2 = new PuntoDeControl(1f,2f);
+		PuntoDeControl punto3 = new PuntoDeControl(0.4f,1.5f);
+		PuntoDeControl punto4 = new PuntoDeControl(0.4f,0f);
+		//ult2
+		PuntoDeControl punto5 = new PuntoDeControl(3f,20f);
+		PuntoDeControl punto6 = new PuntoDeControl(1f,9f);
+		PuntoDeControl punto7 = new PuntoDeControl(2f,4.5f);
+		PuntoDeControl punto8 = new PuntoDeControl(1.7f,3f);
+		
+		list.add(punto1);
+		list.add(punto2);
+		list.add(punto3);
+		list.add(punto4);
+		
+		list.add(punto5);
+		list.add(punto6);
+		list.add(punto7);
+		list.add(punto8);
+		
+		this.sup = new SuperficieDeRevolucion(list);
 	}
 
 	public void setPorcentajelLlenado(float porcentaje){
@@ -82,7 +111,8 @@ public class Botella  implements Dibujable,Animable {
 			gl.glTranslatef(this.posicion.getX(), 0.0f, 0.0f);
 			glut.glutSolidCylinder(0.1f, 0.5f, 20, 20);
 		gl.glPopMatrix();	
-			
+
+//		this.sup.dibujar(gLDrawable);
 
 			
 	}
