654712c4135c9cd90eb00e7b77f33cc71a873f74
gustavo.g.negri@gmail.com <gustavo.g.negri@gmail.com@475089f5-3604-efdd-6849-81d60c1289cb>
2012-04-21 03:12:27 +0000

    Se agregaron algunos atributos de la abertura. Se dibuja la abertura

--- trunk/src/objetosEscena/Dispenser.java
+++ trunk/src/objetosEscena/Dispenser.java
@@ -2,6 +2,7 @@ package objetosEscena;
 
 import java.util.Observable;
 
+import javax.media.opengl.GL;
 import javax.media.opengl.GL2;
 import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.glu.GLU;
@@ -23,6 +24,9 @@ public class Dispenser extends Observable implements Dibujable {
 	private float anchoAbertura = ancho/4f;
 	private float altoAbertura = alto/4f;
 	private float profundidadAbertura = largo/8f;
+	private float separacionAberturaX = ancho/6;	// alejamiento de la abertura con respecto al borde del dispenser
+	private float separacionAberturaY = largo/4;	// alejamiento de la abertura con respecto a la base del dispenser
+	
 	private ManejoShaders2 shader;
 	GLU glu;
 	GLAutoDrawable gLDrawable;
@@ -50,8 +54,48 @@ public class Dispenser extends Observable implements Dibujable {
 		//System.out.println("Se dibujo dispenser");
 		final GL2 gl = gLDrawable.getGL().getGL2();
 		gl.glPushMatrix();
+			gl.glScalef(2, 2, 2);
 			gl.glTranslatef(this.posicion.getX(), 0.0f, 0.0f);
-			glut.glutSolidCube(0.5f);
+			//glut.glutSolidCube(0.5f);
+			gl.glPushMatrix();	// dibujo abertura
+				gl.glTranslated(separacionAberturaX, separacionAberturaY,0);
+				
+				// PARTE IZQUIERDA
+				gl.glBegin(GL.GL_LINE_STRIP);
+				gl.glVertex3f(0, 0,0);
+				gl.glVertex3f(0, 0, -profundidadAbertura);
+				gl.glVertex3f(0, altoAbertura,-profundidadAbertura);
+				gl.glVertex3f(0, altoAbertura,0);
+				gl.glVertex3f(0, 0,0);
+				gl.glEnd();
+				
+				//PARTE SUPERIOR
+				gl.glBegin(GL.GL_LINE_STRIP);
+				gl.glVertex3f(0, altoAbertura,-profundidadAbertura);
+				gl.glVertex3f(0, altoAbertura,0);
+				gl.glVertex3f(anchoAbertura, altoAbertura,0);
+				gl.glVertex3f(anchoAbertura, altoAbertura,-profundidadAbertura);
+				gl.glVertex3f(0, altoAbertura,-profundidadAbertura);
+				gl.glEnd();
+				
+				//PARTE DERECHA
+				gl.glBegin(GL.GL_LINE_STRIP);
+				gl.glVertex3f(anchoAbertura, 0,0);
+				gl.glVertex3f(anchoAbertura, 0, -profundidadAbertura);
+				gl.glVertex3f(anchoAbertura, altoAbertura,-profundidadAbertura);
+				gl.glVertex3f(anchoAbertura, altoAbertura,0);
+				gl.glVertex3f(anchoAbertura, 0,0);
+				gl.glEnd();
+				
+				//PARTE INFERIOR
+				gl.glBegin(GL.GL_LINE_STRIP);
+				gl.glVertex3f(0, 0,-profundidadAbertura);
+				gl.glVertex3f(0, 0,0);
+				gl.glVertex3f(anchoAbertura, 0,0);
+				gl.glVertex3f(anchoAbertura, 0,-profundidadAbertura);
+				gl.glVertex3f(0, 0,-profundidadAbertura);
+				gl.glEnd();
+			gl.glPopMatrix();
 			
 		gl.glPopMatrix();	
 
