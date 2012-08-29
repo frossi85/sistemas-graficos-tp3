d5e15a11c871ab5cd19720ecbf46a8936b579385
gustavo.g.negri <gustavo.g.negri@gmail.com>
2012-08-29 01:06:23 +0000

    Comente la carga de texturas para mejorar rendimiento

--- src/objetosEscena/Botella.java
+++ src/objetosEscena/Botella.java
@@ -45,36 +45,31 @@ public class Botella  implements Dibujable,Animable {
 		this.gl = gLDrawable.getGL().getGL2();
 		this.glu = glu;
 		this.glut = glut;
-		texturaCubica = new TexturaCubeMap(gl, glu, 512);
+		//texturaCubica = new TexturaCubeMap(gl, glu, 512);
 
-		texturaCubica.cargarXPositivo("lib/textura_pared.jpg");
-		texturaCubica.cargarYPositivo("lib/textura_pared.jpg");
-	  	texturaCubica.cargarZPositivo("lib/textura_pared.jpg");
-	  	texturaCubica.cargarXNegativo("lib/textura_pared.jpg");
-	  	texturaCubica.cargarYNegativo("lib/textura_pared.jpg");
-	  	texturaCubica.cargarZNegativo("lib/textura_piso.jpg");
+		//texturaCubica.cargarXPositivo("lib/textura_pared.jpg");
+		//texturaCubica.cargarYPositivo("lib/textura_pared.jpg");
+	  	//texturaCubica.cargarZPositivo("lib/textura_pared.jpg");
+	  	//texturaCubica.cargarXNegativo("lib/textura_pared.jpg");
+	  	//texturaCubica.cargarYNegativo("lib/textura_pared.jpg");
+	  	//texturaCubica.cargarZNegativo("lib/textura_piso.jpg");
 		
 		this.shader = shader;
 	  	this.lleno = false;
 		this.etiquetado = false;
 		this.posicion = new Vertice(0f,0f,0f);
 		ArrayList<PuntoDeControl>list = new ArrayList<PuntoDeControl>();
-		
-		
-		//opcion2
-		/*PuntoDeControl punto1 = new PuntoDeControl(1.7f,3f);
-		PuntoDeControl punto2 = new PuntoDeControl(1f,2f);
+		//opcion1	los primeros puntos de la lista tienen las coord Y mas altas (esto lo usa el calculo de altura)
+		/*PuntoDeControl punto1 = new PuntoDeControl(1.1f,3f);
+		PuntoDeControl punto2 = new PuntoDeControl(0.6f,2f);
 		PuntoDeControl punto3 = new PuntoDeControl(0.4f,1.5f);
 		PuntoDeControl punto4 = new PuntoDeControl(0.4f,0f);
-	
-		PuntoDeControl punto5 = new PuntoDeControl(3f,20f);
-		PuntoDeControl punto6 = new PuntoDeControl(1f,9f);
-		PuntoDeControl punto7 = new PuntoDeControl(2f,4.5f);
-		PuntoDeControl punto8 = new PuntoDeControl(1.7f,3f);
+		
+		PuntoDeControl punto5 = new PuntoDeControl(2.1f,20f);
+		PuntoDeControl punto6 = new PuntoDeControl(5.1f,9f);
+		PuntoDeControl punto7 = new PuntoDeControl(1.4f,4.5f);
+		PuntoDeControl punto8 = new PuntoDeControl(1.1f,3f);
 		*/
-		
-		
-		//opcion1	los primeros puntos de la lista tienen las coord Y mas altas (esto lo usa el calculo de altura)
 		PuntoDeControl punto1 = new PuntoDeControl(1.1f,3f);
 		PuntoDeControl punto2 = new PuntoDeControl(0.6f,2f);
 		PuntoDeControl punto3 = new PuntoDeControl(0.4f,1.5f);
@@ -150,37 +145,16 @@ public class Botella  implements Dibujable,Animable {
 		
 	@Override
 	public void dibujar(GLAutoDrawable gLDrawable) {
-		//System.out.println("Se dibujo botella");
-		texturaCubica.habilitar();
+		//texturaCubica.habilitar();
 		int uniloc = -1;
 		 //SETEO LA TEXTURA DEL UNIFORM cubeMap A LA UNIDAD DE TEXTURA 0 
         uniloc = gl.glGetUniformLocation(shader.getProgramHandler(), "cubeMap"); 
         if( uniloc >= 0 ) 
             gl.glUniform1i(uniloc, texturaCubica.cubemap);   
-
-		final GL2 gl = gLDrawable.getGL().getGL2();
-		
-		//gl.glRotatef(180, 0, 0, 1);
+        final GL2 gl = gLDrawable.getGL().getGL2();
 		gl.glTranslatef(this.posicion.getX(),0, 0.0f);
-		//gl.glScalef(0.3f, 0.3f, 0.3f);
-		gl.glBegin(GL.GL_LINES);
-			//gl.glVertex3d(0,0,0);
-			//gl.glVertex3d(0,-10,0);
-			//gl.glVertex3d(norm.getX(),norm.getY(),norm.getZ());
-			gl.glEnd();
-			//gl.glTranslatef(this.posicion.getX(), -altura/2, 0.0f);
-			//gl.glRotatef(90f, 1f, 0f, 0f);
-			
-			this.sup.dibujar(gLDrawable);
-			/*
-			gl.glBegin(GL.GL_TRIANGLES);
-			gl.glNormal3f(1, 0, 0);
-			gl.glVertex3f(0, 1,0);
-			gl.glVertex3f(0, 0,1);
-			gl.glVertex3f(0, 0,0);
-			gl.glEnd();
-			*/
-		gl.glPopMatrix();	
+		this.sup.dibujar(gLDrawable);
+		//gl.glPopMatrix();	
 	}
 
 	@Override
