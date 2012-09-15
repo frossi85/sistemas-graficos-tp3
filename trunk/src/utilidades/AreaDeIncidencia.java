14493a0caaebc2e4a3bd6bc226316e7bf8f1808e
gustavo.g.negri@gmail.com <gustavo.g.negri@gmail.com@475089f5-3604-efdd-6849-81d60c1289cb>
2012-09-15 21:42:16 +0000

    Se corrigieron metodos y se agrego getArea()

--- trunk/src/utilidades/AreaDeIncidencia.java
+++ trunk/src/utilidades/AreaDeIncidencia.java
@@ -6,69 +6,87 @@ public class AreaDeIncidencia {
 	private Vertice vertice1;
 	private Vertice vertice2;
 	private Vertice vertice3;
+	private Vertice vertice4;
 	private float [] maximos;	// x,y,z maximos respectivamente
 	private float [] minimos; 	// x,y,z minimos respectivamente
 	
-	public AreaDeIncidencia(Vertice vert1, Vertice vert2, Vertice vert3){	// consideramos el caso en q el area es un plano. (se genera con 3 puntos)
+	public AreaDeIncidencia(Vertice vert1, Vertice vert2, Vertice vert3, Vertice vert4){	// consideramos el caso en q el area es un plano. (se genera con 3 puntos)
 		
 		vertice1 = vert1;
 		vertice2 = vert2;
 		vertice3 = vert3;
+		vertice4 = vert4;
 		
 		maximos = new float[3];
 		minimos = new float[3];
 		
-		maximos[0] = getMaximo(vert1.getX(), vert2.getX(), vert3.getX());
-		maximos[1] = getMaximo(vert1.getY(), vert2.getY(), vert3.getY());
-		maximos[2] = getMaximo(vert1.getZ(), vert2.getZ(), vert3.getZ());
+		maximos = getMaximos(vert1, vert2, vert3, vert4);
+		minimos = getMinimos(vert1, vert2, vert3, vert4);
 		
-		minimos[0] = getMinimo(vert1.getX(), vert2.getX(), vert3.getX());
-		minimos[1] = getMinimo(vert1.getY(), vert2.getY(), vert3.getY());
-		minimos[2] = getMinimo(vert1.getZ(), vert2.getZ(), vert3.getZ());
 				
 	}
 	
-	public boolean estaAdentro(Vertice p1, Vertice p2, Vertice p3){
+	public boolean estaAdentro(Vertice p1, Vertice p2, Vertice p3, Vertice p4){
 		float maxTemp[] = new float [3];	// representan los max y min del area pasada por parametro
 		float minTemp[] = new float [3];
 		
-		maxTemp = getMaximos(p1, p2, p3);
-		minTemp = getMinimos(p1, p2, p3);
+		maxTemp = getMaximos(p1, p2, p3, p4);
+		minTemp = getMinimos(p1, p2, p3, p4);
 		
 		return(estaAdentroX(maxTemp[0], minTemp[0]) && (estaAdentroY(maxTemp[1], minTemp[1])) && (estaAdentroZ(maxTemp[2], minTemp[2])));
 	}
 	
-	float[] getMaximos(Vertice p1, Vertice p2, Vertice p3){
+	float[] getMaximos(Vertice p1, Vertice p2, Vertice p3, Vertice p4){
 		float max[] = new float[3];
-		max[0] = getMaximo(p1.getX(), p2.getX(), p3.getX());
-		max[1] = getMaximo(p1.getY(), p2.getY(), p3.getY());
-		max[2] = getMaximo(p1.getZ(), p2.getZ(), p3.getZ());
+		max[0] = getMaximo(p1.getX(), p2.getX(), p3.getX(),  p4.getX());
+		max[1] = getMaximo(p1.getY(), p2.getY(), p3.getY(), p4.getY());
+		max[2] = getMaximo(p1.getZ(), p2.getZ(), p3.getZ(), p4.getZ());
 		return max;		
 	}
 	
-	float getMaximo(float a, float b, float c){
+	float getMaximo(float a, float b, float c, float d){
 		if(a >= b){
-			if(a >= c)return a; return c;
+			if(a >= c){
+				if(a >= d) return a; return d;
+			}
+			else{
+				if(c >= d) return c; return d;
+			}
 		}
 		else{
-			if(b >= c) return b; return c;
+			if(b >= c) {
+				if(b >= d) return b; return d; 
+			}
+			else{
+				if(c >= d) return c; return d;
+			}
 		}
 	}
 	
-	float[] getMinimos(Vertice p1, Vertice p2, Vertice p3){
+	float[] getMinimos(Vertice p1, Vertice p2, Vertice p3, Vertice p4){
 		float min[] = new float[3];
-		min[0] = getMinimo(p1.getX(), p2.getX(), p3.getX());
-		min[1] = getMinimo(p1.getY(), p2.getY(), p3.getY());
-		min[2] = getMinimo(p1.getZ(), p2.getZ(), p3.getZ());
+		min[0] = getMinimo(p1.getX(), p2.getX(), p3.getX(), p4.getX());
+		min[1] = getMinimo(p1.getY(), p2.getY(), p3.getY(), p4.getY());
+		min[2] = getMinimo(p1.getZ(), p2.getZ(), p3.getZ(), p4.getZ());
 		return min;		
 	}
 	
-	float getMinimo(float a, float b, float c){
+	float getMinimo(float a, float b, float c, float d){
 		if(a <= b){
-			if(a <= c)return a; return c;
+			if(a <= c){
+				if(a <= d) return a; return d;
+			}
+			else{
+				if(c <= d) return c; return d;
+			}
 		}
 		else{
-			if(b <= c) return b; return c;
+			if(b <= c) {
+				if(b <= d) return b; return d; 
+			}
+			else{
+				if(c <= d) return c; return d;
+			}
 		}
 	}
 	
@@ -82,6 +100,14 @@ public class AreaDeIncidencia {
 	
 	boolean estaAdentroZ(float zMax, float zMin){
 		return ((zMax <= maximos[2]) && (zMin >= minimos[2]));
+	}
+	
+	public Vertice[] getArea(){
+		Vertice resultado[] = new Vertice [4];
+		resultado[0] = vertice1;
+		resultado[2] = vertice2;
+		resultado[3] = vertice4;
+		return resultado;
 	}
 
 }
